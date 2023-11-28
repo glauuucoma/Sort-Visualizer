86121b0fb14aa2f3618660da74ecf59e145ad4b8
Davyd Kuleba <kulebadavid@gmail.com>
2023-11-28 00:04:13 -0500

    Add display method and create rectangles

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import pygame
 import random
 pygame.init()
 
-class DrawInfo:
+class DrawInformation:
     BLACK = 0,0,0 # COLORS ARE DEFINED IN RGB
     WHITE = 2,5,5
     GREEN = 0,255,0
     RED  = 255,0,0
-    GREY = 128,128,128
     BACKGROUND_COLOR = WHITE
 
-    SIDE_PAD = 100
-    TOP = 150
+    GRADIENTS = [
+        (128,128,128),
+        (160,160,160),
+        (192,192,192)
+    ]
 
-    def __init__(self,width,height,list):
+    SIDE_PAD = 100
+    TOP_PAD = 150
+
+    def __init__(self,width,height,lst):
         self.width = width
         self.height = height
 
@@ -26,5 +31,54 @@ class DrawInfo:
         self.min_value = min(lst)
         self.max_value = max(lst)
 
-        self.block_width = round((self.width - self.SIDE_PAD) / len(lst)) #14.30
+        self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
         self.block_height = round((self.height - self.TOP_PAD) / (self.max_value - self.min_value))
+        self.start_x = self.SIDE_PAD // 2
+
+
+def draw(draw_info):
+    draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+    pygame.display.update()
+
+def draw_list(draw_info):
+    lst = draw_info.lst
+    
+    for i, val in enumerate(lst):
+        x = draw_info.start_x + i * draw_info.block_width
+        y = val * draw_info.block_height
+
+
+def generate_starting_list(n,min_val,max_val):
+    lst = []
+
+    for _ in range(n):
+        val = random.randint(min_val, max_val)
+        lst.append(val)
+
+    return lst
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    n = 50
+    min_val = 0
+    max_val = 100
+
+    lst = generate_starting_list(n, min_val, max_val)
+    draw_info = DrawInformation(800,600, lst)
+
+    while run:
+        clock.tick(60)
+        
+        draw(draw_info)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
