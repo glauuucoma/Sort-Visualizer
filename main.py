c12b21bf66380b685a077eac35bacb4bb8df90e2
Davyd Kuleba <kulebadavid@gmail.com>
2023-11-28 00:27:11 -0500

    add keyboard listeners

--- main.py
+++ main.py
@@ -1,84 +1,107 @@
 import pygame
 import random
+import math
 pygame.init()
 
 class DrawInformation:
-    BLACK = 0,0,0 # COLORS ARE DEFINED IN RGB
-    WHITE = 2,5,5
-    GREEN = 0,255,0
-    RED  = 255,0,0
-    BACKGROUND_COLOR = WHITE
+	
+    # Color implementaton
+	BLACK = 0, 0, 0
+	WHITE = 255, 255, 255
+	GREEN = 0, 255, 0
+	RED = 255, 0, 0
+	BACKGROUND_COLOR = WHITE
+	GRADIENTS = [
+		(128, 128, 128),
+		(160, 160, 160),
+		(192, 192, 192)
+	]
+	
+    #Paddings
+	SIDE_PAD = 100
+	TOP_PAD = 150
 
-    GRADIENTS = [
-        (128,128,128),
-        (160,160,160),
-        (192,192,192)
-    ]
+	def __init__(self, width, height, lst):
+		self.width = width
+		self.height = height
 
-    SIDE_PAD = 100
-    TOP_PAD = 150
+		self.window = pygame.display.set_mode((width, height))
+		pygame.display.set_caption("Sorting Algorithm Visualization")
+		self.set_list(lst)
 
-    def __init__(self,width,height,lst):
-        self.width = width
-        self.height = height
+	def set_list(self, lst):
+		self.lst = lst
+		self.min_val = min(lst)
+		self.max_val = max(lst)
 
-        self.window = pygame.display.set_mode((width, height))
-        pygame.display.set_caption("Sorting Algorithm Visualizer")
-        self.set_list(lst)
-
-    def set_list(self, lst):
-        self.lst = lst
-        self.min_value = min(lst)
-        self.max_value = max(lst)
-
-        self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
-        self.block_height = round((self.height - self.TOP_PAD) / (self.max_value - self.min_value))
-        self.start_x = self.SIDE_PAD // 2
+		self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
+		self.block_height = math.floor((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+		self.start_x = self.SIDE_PAD // 2
 
 
 def draw(draw_info):
-    draw_info.window.fill(draw_info.BACKGROUND_COLOR)
-    pygame.display.update()
+	draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+	draw_list(draw_info)
+	pygame.display.update()
+
 
 def draw_list(draw_info):
-    lst = draw_info.lst
-    
-    for i, val in enumerate(lst):
-        x = draw_info.start_x + i * draw_info.block_width
-        y = val * draw_info.block_height
+	lst = draw_info.lst
+
+	for i, val in enumerate(lst):
+		x = draw_info.start_x + i * draw_info.block_width
+		y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+		color = draw_info.GRADIENTS[i % 3] # Generate random color using modulo operator
+		pygame.draw.rect(draw_info.window, color, (x,y, draw_info.block_width, draw_info.height))
 
 
-def generate_starting_list(n,min_val,max_val):
-    lst = []
+def generate_starting_list(n, min_val, max_val):
+	lst = []
 
-    for _ in range(n):
-        val = random.randint(min_val, max_val)
-        lst.append(val)
+	for _ in range(n):
+		val = random.randint(min_val, max_val)
+		lst.append(val)
 
-    return lst
-
+	return lst
 
 def main():
-    run = True
-    clock = pygame.time.Clock()
+	run = True
+	clock = pygame.time.Clock()
 
-    n = 50
-    min_val = 0
-    max_val = 100
+	n = 50
+	min_val = 0
+	max_val = 100
 
-    lst = generate_starting_list(n, min_val, max_val)
-    draw_info = DrawInformation(800,600, lst)
+	lst = generate_starting_list(n, min_val, max_val)
+	draw_info = DrawInformation(800, 600, lst)
+	ascending = True
+	sorting = False
 
-    while run:
-        clock.tick(60)
-        
-        draw(draw_info)
+	while run:
+		clock.tick(60)
+		draw(draw_info)
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = false
+			if event.type != pygame.KEYDOWN:
+				continue
+			if event.key == pygame.K_r:
+				lst = generate_starting_list(n, min_val, max_val)
+				draw_info.set_list(lst)
+				sorting == False
+			elif event.key == pygame.K_SPACE and sorting == False:
+				sorting = True
+			elif event.key == pygame.K_a and not sorting:
+				ascending = True
+			elif event.key == pygame.K_d and not sorting:
+				ascending = False
+			
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-    
-    pygame.quit()
+
+		
+	pygame.quit()
+
 
 if __name__ == "__main__":
-    main()
+	main()
